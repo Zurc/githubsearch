3088b393535872d259c4fe3d66b336bc18c538fb
Zurc <cruzjurado@gmail.com>
2017-01-22 07:55:44 +0000

    profile component created

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <gs-profile></gs-profile>
+  `,
 })
 export class AppComponent  { name = 'Angular'; }
---
+++ app/components/profile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'gs-profile',
+  templateUrl : `profile.component.html`,
+})
+export class ProfileComponent  { }
